80d6178fec4762a6e0b01026fb078ebd76c9f675
Hari Nair <harivn786@gmail.com>
2020-09-14 20:17:36 +0530

    first

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_oih2au=pqvaj)kdkw7_px*-xpm%crmv(@4oepqvr*^8un8td)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['harryvn.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
